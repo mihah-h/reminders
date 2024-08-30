942e87770135a75182e3b5374335b378b3a85e7b
Misha <gmike103g@gmail.com>
2024-08-31 02:28:50 +0500

    reminder list table has been created

--- src/app/core/services/reminders.service.ts
+++ src/app/core/services/reminders.service.ts
@@ -1,12 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Reminder } from '../models/reminder';
 import { Status } from '../models/status';
+import { STATUS_NAME } from '../models/status-name';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RemindersService {
-  private reminders: Reminder[] = [];
+  private reminders: Reminder[] = [
+    {
+      shortDescription: 'Купить молоко',
+      fullDescription: 'Купить 1 литр молока в магазине на углу',
+      dueDateTime: new Date(2024, 3, 15, 18, 0),
+      status: new Status(STATUS_NAME.COMPLETED),
+      creationDateTime: new Date(2023, 11, 12, 10, 15),
+    },
+    {
+      shortDescription: 'Заплатить за интернет',
+      fullDescription: 'Оплатить счет за интернет до 15 апреля',
+      dueDateTime: new Date(2024, 3, 15, 23, 59),
+      status: new Status(STATUS_NAME.EXPIRED),
+      creationDateTime: new Date(2023, 10, 19, 15, 30),
+    },
+    {
+      shortDescription: 'Встретиться с другом',
+      fullDescription: 'Встреча с другом в кафе "Кофемания" в 19:00',
+      dueDateTime: new Date(2024, 3, 18, 19, 0),
+      status: new Status(STATUS_NAME.COMPLETED),
+      creationDateTime: new Date(2024, 2, 10, 12, 45),
+    },
+    {
+      shortDescription: 'Позвонить родителям',
+      fullDescription: 'Позвонить родителям и поздравить с днем рождения',
+      dueDateTime: new Date(2024, 3, 20, 12, 0),
+      status: new Status(STATUS_NAME.COMPLETED),
+      creationDateTime: new Date(2024, 1, 15, 11, 0),
+    },
+    {
+      shortDescription: 'Сдать отчет',
+      fullDescription: 'Сдать отчет по проекту "Аналитика" до конца недели',
+      dueDateTime: new Date(2024, 3, 22, 23, 59),
+      status: new Status(STATUS_NAME.NEW),
+      creationDateTime: new Date(2024, 3, 10, 9, 30),
+    },
+    {
+      shortDescription: 'Записаться на прием',
+      fullDescription: 'Записаться на прием к врачу',
+      dueDateTime: new Date(2024, 3, 25, 10, 0),
+      status: new Status(STATUS_NAME.SCHEDULES),
+      creationDateTime: new Date(2024, 3, 23, 14, 15),
+    },
+    {
+      shortDescription: 'Купить билеты на концерт',
+      fullDescription: 'Купить билеты на концерт любимой группы',
+      dueDateTime: new Date(2024, 4, 5, 20, 0),
+      status: new Status(STATUS_NAME.NEW),
+      creationDateTime: new Date(2024, 2, 25, 16, 0),
+    },
+  ];
 
   constructor() {}
 
--- src/app/pages/reminder-list/reminder-list.component.ts
+++ src/app/pages/reminder-list/reminder-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import {
   MatCell, MatCellDef,
   MatColumnDef,
@@ -6,29 +6,16 @@ import {
   MatHeaderRow,
   MatHeaderRowDef,
   MatRow, MatRowDef,
-  MatTable
+  MatTable, MatTableDataSource
 } from '@angular/material/table';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
+import { MatSort, MatSortHeader, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { RemindersService } from '../../core/services/reminders.service';
+import { Reminder } from '../../core/models/reminder';
+import { MatPaginator } from '@angular/material/paginator';
+import { DatePipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatFabButton, MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-reminder-list',
@@ -43,12 +30,43 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatHeaderRow,
     MatRowDef,
     MatCellDef,
-    MatHeaderCellDef
+    MatHeaderCellDef,
+    MatSortHeader,
+    MatSort,
+    DatePipe,
+    MatIcon,
+    MatFabButton,
+    MatIconButton
   ],
   templateUrl: './reminder-list.component.html',
   styleUrl: './reminder-list.component.scss'
 })
-export class ReminderListComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class ReminderListComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['shortDescription', 'creationDateTime', 'dueDateTime', 'status', 'delete'];
+  reminders: Reminder[] = [];
+  dataSource!: MatTableDataSource<Reminder> ;
+
+  constructor(
+    public remindersService: RemindersService,
+    private _liveAnnouncer: LiveAnnouncer,
+  ) {}
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit() {
+    this.reminders = this.remindersService.getReminders();
+    this.dataSource = new MatTableDataSource(this.reminders);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
